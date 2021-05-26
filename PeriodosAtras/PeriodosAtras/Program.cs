4e5352172607ec578b437556870319ae8880c531
Caroline Lara <40472514+Lar089@users.noreply.github.com>
2021-05-26 17:19:50 -0300

    Add files via upload

---
+++ PeriodosAtras/PeriodosAtras/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PeriodosAtras.Periodo;
+
+namespace PeriodosAtras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Humunizar(DateTime.Now, DateTime.Parse("11/11/2011")).valor_saida);
+        }
+    }
+}
